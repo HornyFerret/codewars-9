423468b7d5cc802d6df2f341c313a8d581da57aa
HornyFerret <ombro.de.morto@gmail.com>
2021-07-11 20:17:26 +0300

    Добавьте файлы проекта.

---
+++ codewars-9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace codewars_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("------");
+            int w = СenturyFromYear(year);
+            Console.WriteLine(w);
+        }
+        public static int СenturyFromYear(int year)
+        {
+            int cen = year % 100;
+            if (cen>0)
+            {
+                int r = (year / 100) + 1;
+                return r;
+            }
+            else if (cen == 0)
+            {
+                int r = year / 100;
+                return r;
+            }
+            else
+            {
+                return year;
+            }
+        }
+        
+    }
+}
